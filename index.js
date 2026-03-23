eb732ced5163e1e3323a7838edb64d472cc6da2c
HikariLucky <vx.hikari_yt16@hotmail.com>
2026-03-23 10:18:25 -0300

    Criando o post do produto

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const router = express.Router()
 const app = express();
 const port = 3000;
 
-app.get("/", async (res, req) => {
+app.get("/produto", async (res, req) => {
     fs.readFile('data.json', 'utf-8', (err, data) => {
         if (err) {
             return res.statusCode(500).json({erro: 'Erro ao ler o arquivo'});
@@ -17,7 +17,7 @@ app.get("/", async (res, req) => {
     });
 });
 
-router.get("/:id", async (res, req) => {
+router.get("/produto/:id", async (res, req) => {
     try {
         const data = await fs.readFile('data.json', 'utf-8');
         const json = JSON.parse(data);
@@ -33,6 +33,35 @@ router.get("/:id", async (res, req) => {
     } catch {
         res.status(500).json({erro: 'Erro interno'})
     }
+});
+
+app.post('/produto', async (res, req) => {
+    try{
+        const data = await fs.readFile('data.json', 'utf-8');
+        const json = JSON.parse(data);
+
+        const {nome, preco} = req.body;
+        
+        if (!nome || !preco) {
+            return res.status(400).json({erro: 'Nome e preço são obrigatórios'})
+        }
+
+        const novoId = json.produto.length > 0 ? json.produto[json.produto.length - 1].id + 1 : 1;
+
+        const novoProduto = {
+            id: novoId,
+            nome,
+            preco
+        };
+
+        json.produto.push(novoProduto);
+        await fs.writeFile('./data.json', JSON.stringify(json, null, 2));
+
+        res.status(201).json(novoProduto)
+    } catch {
+        res.status(500).json({erro: 'Erro ao criar o produto'})
+    }
+    
 });
 
 app.listen(port, () => {
